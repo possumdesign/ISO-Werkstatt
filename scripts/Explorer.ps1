8eee778ddd0a8eae5f7d38e6e96ba99fa0946912
possumdesign <possumdesign@users.noreply.github.com>
2026-09-08 15:54:49 +0200

    v0.4.3 stabile Windows 11 Pro VM Baseline

---
+++ scripts/Explorer.ps1
@@ -0,0 +1,30 @@
+﻿# Explorer-Defaults für LabAdmin
+
+$ExplorerAdvanced = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+
+# Der Schlüssel existiert in Windows bereits.
+# Absichtlich kein New-Item hier: Das hatte beim FirstLogon zu UnauthorizedAccessException geführt.
+
+# Dateiendungen anzeigen
+Set-ItemProperty `
+    -Path $ExplorerAdvanced `
+    -Name "HideFileExt" `
+    -Value 0
+
+# Versteckte Dateien anzeigen
+Set-ItemProperty `
+    -Path $ExplorerAdvanced `
+    -Name "Hidden" `
+    -Value 1
+
+# Geschützte Systemdateien weiterhin versteckt
+Set-ItemProperty `
+    -Path $ExplorerAdvanced `
+    -Name "ShowSuperHidden" `
+    -Value 0
+
+# Explorer startet mit "Dieser PC"
+Set-ItemProperty `
+    -Path $ExplorerAdvanced `
+    -Name "LaunchTo" `
+    -Value 1
